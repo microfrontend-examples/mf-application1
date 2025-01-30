0de1ebb70b36631b7e79ebb7e7732b1deecee186
Annas Alghoffar <aag.annas@gmail.com>
2025-01-30 14:38:18 +0700

    feat: add development-spa

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,24 @@ import { defineConfig } from "vite"
 import vitePluginSingleSpa from "vite-plugin-single-spa";
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
-export default defineConfig({
-  plugins: [react(), TanStackRouterVite(), vitePluginSingleSpa({
-    type: 'mife',
-    projectId: 'mf-application1',
-    serverPort: 4175,
-    spaEntryPoints: 'src/spa.tsx',
-  })],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({mode}) => {
+  // for simultaneous running development, you need disable HMR.
+  const isDevSpa = mode === 'development-spa';
+
+  return ({
+    plugins: [react(), TanStackRouterVite(), vitePluginSingleSpa({
+      type: 'mife',
+      projectId: 'mf-application1',
+      serverPort: 4175,
+      spaEntryPoints: 'src/spa.tsx',
+    })],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    server: {
+      hmr: !isDevSpa
+    }
+  })
 })
