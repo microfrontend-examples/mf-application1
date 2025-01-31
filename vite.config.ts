9439f4af2e953dc2395fe934ca81bcf2396fdbaa
Annas Alghoffar <aag.annas@gmail.com>
2025-02-01 01:10:52 +0700

    feat: update config and env

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,27 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import {defineConfig, loadEnv} from "vite"
 import vitePluginSingleSpa from "vite-plugin-single-spa";
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import {TanStackRouterVite} from '@tanstack/router-plugin/vite'
 
 export default defineConfig(({mode}) => {
   // for simultaneous running development, you need disable HMR.
   const isDevSpa = mode === 'development-spa';
 
+  const env = loadEnv(mode, process.cwd(), "");
+
   return ({
+    server: {
+      hmr: !isDevSpa,
+      port: Number(env.VITE_PORT),
+    },
+    preview: {
+      port: Number(env.VITE_PORT),
+    },
     plugins: [react(), TanStackRouterVite(), vitePluginSingleSpa({
       type: 'mife',
       projectId: 'mf-application1',
-      serverPort: 4175,
+      serverPort: Number(env.VITE_PORT),
       spaEntryPoints: 'src/spa.tsx',
     })],
     resolve: {
@@ -20,8 +29,5 @@ export default defineConfig(({mode}) => {
         "@": path.resolve(__dirname, "./src"),
       },
     },
-    server: {
-      hmr: !isDevSpa
-    }
   })
 })
